d97b6675d0a830b8c2ac672f2bcd9db03c2078b9
idealley <samuelpouyt@gmail.com>
2017-12-07 18:33:37 +0100

    fixed linting issue

--- src/hooks/add-id.js
+++ src/hooks/add-id.js
@@ -3,9 +3,7 @@
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
-    console.log(hook)
     const user = hook.params.user;
-    console.log(user)
     hook.data._id = user._id;
     hook.data.ersId = user.ersId;
     return Promise.resolve(hook);
