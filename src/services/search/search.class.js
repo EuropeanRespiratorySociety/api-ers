83d407c71ab08aa75e74b776626b2c11bfea516a
idealley <samuelpouyt@gmail.com>
2018-03-08 16:38:44 +0100

    some tests

--- src/services/search/search.class.js
+++ src/services/search/search.class.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-const es = require('../../helpers/elastic.js');
+const { client } = require('../../helpers/elastic.js');
 
 class Service {
   constructor (options) {
@@ -7,8 +7,27 @@ class Service {
   }
 
   async find (params) {
-    const query = params.query || {};
-    return [];
+    const q = params.query || {};
+    const i = q.i || 'all';
+
+    const results = await client.search({
+      index: indices(i),
+      body: query(q.q, 0)
+    });
+
+    const r = results.hits.hits.map(i => {
+      const c = i._source;
+      return {
+        title: c.title,
+        access: c.access || false,
+        url: c.url || c.canonical,
+        journal: c.journal_name || false,
+        category: c.category ? c.category.title  : false,
+        authors: c.authors ? c.authors.join(', ') : false
+      };
+    });
+    return { results: r, aggs: results.aggregations, total: results.hits.total };
+
   }
 
   // async get (id, params) {
@@ -43,3 +62,61 @@ module.exports = function (options) {
 };
 
 module.exports.Service = Service;
+
+const indices = (param) => {
+  const i = ['content','journals','presentations-congress-2017'];
+  /* eslint-disable indent */
+  return param == 'content'
+  ? i[0]
+  : param == 'journals'
+  ? i[1]
+  : param == 'congress'
+  ? i[2]
+  : i.join(',');
+  /* eslint-enable */
+};
+
+const query = (query, skip) => {
+  return {
+    from: skip, 
+    query: {
+      bool: {
+        should: [
+          {
+            multi_match: {
+              query,
+              fields: ['abstract', 'full_available_text', 'subjects.text']
+            }
+          },
+          {
+            multi_match: {
+              query,
+              fields: ['authorText', 'authors'],
+              boost: 4
+            }
+          },
+          {
+            multi_match: {
+              query,
+              fields: ['body', 'leadParagraph', 'title', 'diseases', 'methods'],
+              boost: 10
+
+            }
+          }
+        ]
+      }
+    },
+    aggs:{ 
+      source: {
+        terms: {
+          field: '_index'
+        }
+      },
+      journals: {
+        terms:{
+          field: 'publisher.keyword'
+        }
+      }
+    }  
+  };
+};
