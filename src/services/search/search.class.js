abf183fc4505120c9c599333f505d886fb469f5b
idealley <samuelpouyt@gmail.com>
2018-03-10 22:13:06 +0100

    intitial working search

--- src/services/search/search.class.js
+++ src/services/search/search.class.js
@@ -17,16 +17,42 @@ class Service {
 
     const r = results.hits.hits.map(i => {
       const c = i._source;
+      const h = i.highlight;
       return {
-        title: c.title,
+        title: hasHighlight(h, c, 'title', 'name' ),
+        lead: hasHighlight(h, c, 'leadParagraph'),
+        type: setType(i._index),
+        _id: i._id,
         access: c.access || false,
         url: c.url || c.canonical,
         journal: c.journal_name || false,
         category: c.category ? c.category.title  : false,
-        authors: c.authors ? c.authors.join(', ') : false
+        /* eslint-disable indent */
+        authors: c.authors 
+          ?  c.authors.join(', ')
+          : c.authorText
+          ? hasHighlight(h, c, 'authorText')
+          : false,
+        speakers: hasHighlight(h, c, 'speakerText'),
+        chairs: hasHighlight(h, c, 'chairText'),
+        abstract: c.Content 
+          ? c.Content.AbstractText 
+          : c.abstract 
+          ? c.abstract 
+          : false,
+        year: c.year || false
+        /* eslint-enable indent */
       };
     });
-    return { results: r, aggs: results.aggregations, total: results.hits.total };
+
+    const aggs = results.aggregations.source.buckets.reduce((a, i) => {
+      if (i.key.includes('journals')) a.journal = i.doc_count;
+      if (i.key.includes('content')) a.web = i.doc_count;
+      if (i.key.includes('congress')) a.congress = i.doc_count;
+      return a;
+    }, {});
+
+    return { results: r, aggs, total: results.hits.total };
 
   }
 
@@ -64,14 +90,14 @@ module.exports = function (options) {
 module.exports.Service = Service;
 
 const indices = (param) => {
-  const i = ['content','journals','presentations-congress-2017'];
+  const i = ['content','journals','presentations-congress-2017', 'sessions-congress-2017'];
   /* eslint-disable indent */
-  return param == 'content'
+  return param == 'web'
   ? i[0]
   : param == 'journals'
   ? i[1]
   : param == 'congress'
-  ? i[2]
+  ? `${i[2]},${i[3]}`
   : i.join(',');
   /* eslint-enable */
 };
@@ -91,7 +117,7 @@ const query = (query, skip) => {
           {
             multi_match: {
               query,
-              fields: ['authorText', 'authors'],
+              fields: ['authorText', 'authors', 'aims', 'audience', 'chairText', 'name', 'subtitle'],
               boost: 4
             }
           },
@@ -106,6 +132,17 @@ const query = (query, skip) => {
         ]
       }
     },
+    // highlight : {
+    //   fields: {
+    //     title : setHighlightTag(),
+    //     name : setHighlightTag(),
+    //     leadParagraph : setHighlightTag(),
+    //     abstract : setHighlightTag(),
+    //     authors : setHighlightTag(),
+    //     authorText : setHighlightTag(),
+    //     achairText : setHighlightTag()
+    //   }
+    // },
     aggs:{ 
       source: {
         terms: {
@@ -120,3 +157,36 @@ const query = (query, skip) => {
     }  
   };
 };
+
+function hasHighlight (highlight, source, key, alt = null) {
+  /* eslint-disable indent */
+  key = source.hasOwnProperty(alt)
+    ? alt
+    : source.hasOwnProperty(key)
+    ? key
+    : false;
+  /* eslint-enable indent */
+  return highlight && highlight.hasOwnProperty(key)
+    ? highlight[key].join(', ')
+    : source[key];
+}
+
+// function setHighlightTag () {
+//   return { pre_tags : ['<span class="highlight">'], post_tags : ['</span>'] };
+// }
+
+function setType (string) {
+  /* eslint-disable indent */
+  return string.includes('journals')
+    ? 'journal' 
+    : string.includes('content')
+    ? 'web'
+    : string.includes('presentations')
+    ? 'presentation'
+    : string.includes('abstracts')
+    ? 'abstracts'
+    : string.includes('sessions')
+    ? 'session'
+    : 'other';
+  /* eslint-enable indent */  
+}
