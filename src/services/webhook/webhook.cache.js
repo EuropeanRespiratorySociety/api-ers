fa4c99228a2e867c22764d986096450d7ae97df9
idealley <samuelpouyt@gmail.com>
2018-07-05 22:43:30 +0200

    trying to find out in prod

--- src/services/webhook/webhook.cache.js
+++ src/services/webhook/webhook.cache.js
@@ -19,17 +19,18 @@ class Cache {
 
   async clear (data) {
     const item = data._cloudcms.node.object;
+    console.log(data)
 
     // special case for events
     if(item.hasOwnProperty('category')) {
-      clearCalendar(this.client, item);
-      clearNews(this.client, item);
+      // clearCalendar(this.client, item);
+      // clearNews(this.client, item);
     }
 
     if(item.hasOwnProperty('category2')) {
       item.category2.map(i => {
-        clearCalendar(this.client, i);
-        clearNews(this.client, i);
+        // clearCalendar(this.client, i);
+        // clearNews(this.client, i);
       });
     }
     
@@ -59,106 +60,106 @@ module.exports = new Cache();
 async function singleItem(client, item, reply, category = false, index = false) {
   const group = reply.cache.group.split('-')[1];
   // temporary, we (try to) clean the ers main website cache
-  const a = axios.post(`https://www.ersnet.org/cache?url=${reply.data.url}`);
-  // the cache is not always busted: @TODO fix it! issue #24 
-  const b = client.get(`/cache/clear/single/${reply.cache.key}`);
-  const c = client.get(`/cache/clear/group/${group}`);
-  const [ta, tb, tc] = await Promise.all([a, b, c]);
+  // const a = axios.post(`https://www.ersnet.org/cache?url=${reply.data.url}`);
+  // // the cache is not always busted: @TODO fix it! issue #24 
+  // const b = client.get(`/cache/clear/single/${reply.cache.key}`);
+  // const c = client.get(`/cache/clear/group/${group}`);
+  // const [ta, tb, tc] = await Promise.all([a, b, c]);
   
-  const result = {
-    timestamp :  m().format('x'),
-    date: new Date(),
-    item: {
-      title: item.title,
-      _doc: item._doc,
-    },
-    website: ta.data,
-    api: {
-      single: tb.data,
-      group: tc.data
-    }
-  }; 
+  // const result = {
+  //   timestamp :  m().format('x'),
+  //   date: new Date(),
+  //   item: {
+  //     title: item.title,
+  //     _doc: item._doc,
+  //   },
+  //   website: ta.data,
+  //   api: {
+  //     single: tb.data,
+  //     group: tc.data
+  //   }
+  // }; 
 
   // eslint-disable-next-line no-console
   console.log(result);
   console.log(group);
   console.log(reply);
   
-  // 1. use cache status (200) to push to log index in ES
-  await es.log('api-webhook-logs', '_doc', result);
-  // 2. fetch new item by API and update the content in ES
-  const req = !category 
-    ? `/${group}/${reply.cache.key}`
-    : `/${group}`;
-  // await client.get(req);
-  // add the new item right away to the cache
-  const article = await client.get(req);
+  // // 1. use cache status (200) to push to log index in ES
+  // await es.log('api-webhook-logs', '_doc', result);
+  // // 2. fetch new item by API and update the content in ES
+  // const req = !category 
+  //   ? `/${group}/${reply.cache.key}`
+  //   : `/${group}`;
+  // // await client.get(req);
+  // // add the new item right away to the cache
+  // const article = await client.get(req);
 
-  console.log('>>>> inspect prod');
+  // console.log('>>>> inspect prod');
 
-  // we parse the item to return minimal data to Elasticsearch
-  const parsed = u.parse(article.data.data);
+  // // we parse the item to return minimal data to Elasticsearch
+  // const parsed = u.parse(article.data.data);
 
-  if(index) {
-    item.unpublished
-      ? await es.unIndex(item)
-      : await es.index(parsed, 'content', parsed._doc);
-  }
+  // if(index) {
+  //   item.unpublished
+  //     ? await es.unIndex(item)
+  //     : await es.index(parsed, 'content', parsed._doc);
+  // }
 
-  // eslint-disable-next-line no-console
-  console.log(chalk.cyan('[webhook]'), `- Cache cleared ${index && !item.unpublished ? 'and item reindexed ' : index && !item.unpublished ? 'and item removed from index' : ''}- [${new Date()}]`);
-  // return temporary object
-  return result;
+  // // eslint-disable-next-line no-console
+  // console.log(chalk.cyan('[webhook]'), `- Cache cleared ${index && !item.unpublished ? 'and item reindexed ' : index && !item.unpublished ? 'and item removed from index' : ''}- [${new Date()}]`);
+  // // return temporary object
+  // return result;
 }
 
-// This is temporary until we change the website
-const urlToBust = [
-  'https://www.ersnet.org'
-];
+// // This is temporary until we change the website
+// const urlToBust = [
+//   'https://www.ersnet.org'
+// ];
 
-const calendarUrlToBust = [
-  'https://www.ersnet.org/congress-and-events/events-calendar?type=deadline',
-  'https://www.ersnet.org/congress-and-events/events-calendar?type=endorsed',
-  'https://www.ersnet.org/congress-and-events/events-calendar?type=non-ers',
-  'https://www.ersnet.org/congress-and-events/events-calendar?type=all'
-]
+// const calendarUrlToBust = [
+//   'https://www.ersnet.org/congress-and-events/events-calendar?type=deadline',
+//   'https://www.ersnet.org/congress-and-events/events-calendar?type=endorsed',
+//   'https://www.ersnet.org/congress-and-events/events-calendar?type=non-ers',
+//   'https://www.ersnet.org/congress-and-events/events-calendar?type=all'
+// ];
   
-function clearCalendar(client, item) {
-  if(item.category.title === 'Events Calendar') {
-    const data = {
-      cache: {
-        key: 'calendar',
-        group: 'group-calendar'
-      },
-      data: {
-        url: 'https://www.ersnet.org/congress-and-events/events-calendar'
-      }
-    };
-    singleItem(client, item, data, true);
-    axios.post(`https://www.ersnet.org/cache?url=${urlToBust[0]}`);
-    axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[0]}`);
-    axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[1]}`);
-    axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[2]}`);
-    axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[3]}`);
-  }
-}
+// function clearCalendar(client, item) {
+//   if(item.category.title === 'Events Calendar') {
+//     const data = {
+//       cache: {
+//         key: 'calendar',
+//         group: 'group-calendar'
+//       },
+//       data: {
+//         url: 'https://www.ersnet.org/congress-and-events/events-calendar'
+//       }
+//     };
+//     singleItem(client, item, data, true);
+//     axios.post(`https://www.ersnet.org/cache?url=${urlToBust[0]}`);
+//     axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[0]}`);
+//     axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[1]}`);
+//     axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[2]}`);
+//     axios.post(`https://www.ersnet.org/cache?url=${calendarUrlToBust[3]}`);
+//   }
+// }
   
-function clearNews(client, item) {
-  if(item.category.title === 'News and Features') {
-    const data = {
-      cache: {
-        key: 'news',
-        group: 'group-news'
-      },
-      data: {
-        url: 'https://www.ersnet.org/the-society/news'
-      }
-    };
-    singleItem(client, item, data, true);
-    // let's clear other urls while thinking about it:
-    axios.post(`https://www.ersnet.org/cache?url=${urlToBust[0]}`)
-  }
-}
+// function clearNews(client, item) {
+//   if(item.category.title === 'News and Features') {
+//     const data = {
+//       cache: {
+//         key: 'news',
+//         group: 'group-news'
+//       },
+//       data: {
+//         url: 'https://www.ersnet.org/the-society/news'
+//       }
+//     };
+//     singleItem(client, item, data, true);
+//     // let's clear other urls while thinking about it:
+//     axios.post(`https://www.ersnet.org/cache?url=${urlToBust[0]}`)
+//   }
+// }
 
 // function clearAppHighlights(client, item) {
 //   if(item.category.id === 'id": "ec586ddd9c918191be2b') {
