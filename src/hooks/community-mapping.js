9fef9e9fc7324a3cbebaf271d74a7e4dd6d4bcec
idealley <samuelpouyt@gmail.com>
2018-07-09 16:52:10 +0200

    from ms to s

--- src/hooks/community-mapping.js
+++ src/hooks/community-mapping.js
@@ -1,5 +1,6 @@
 // Use this hook to manipulate incoming or outgoing data.
 // For more information on hooks see: http://docs.feathersjs.com/api/hooks.html
+const moment = require('moment');
 
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
   return function (hook) {
@@ -12,7 +13,7 @@ module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
         parent_doc_id: i.appCommunity,
         extra_content: i.leadParagraph ? i.leadParagraph : undefined,
         // order: undefined,
-        created_at: i.ms,
+        created_at: moment(i._system.created_on.iso_8601).unix(),
         image_url: i.image,
         post_id: i._doc
       };
