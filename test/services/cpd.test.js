e2a096a8e7c639378e78a6a51f0d60a80f38f821
idealley <samuelpouyt@gmail.com>
2018-08-16 15:39:00 +0200

    Fixed test bug

--- test/services/cpd.test.js
+++ test/services/cpd.test.js
@@ -1,9 +1,9 @@
 const assert = require('assert');
 const app = require('../../src/app');
 
-describe('\'cdp\' service', () => {
+describe('\'cpd\' service', () => {
   it('registered the service', () => {
-    const service = app.service('cdp');
+    const service = app.service('cpd');
 
     assert.ok(service, 'Registered the service');
   });
