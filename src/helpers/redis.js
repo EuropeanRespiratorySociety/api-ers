c96386419830cdd2e4f8ff43ff6aac4d9d10f7a9
idealley <samuelpouyt@gmail.com>
2018-09-16 11:15:42 +0200

    roll back

--- src/helpers/redis.js
+++ src/helpers/redis.js
@@ -1,7 +1,4 @@
 const redis = require('redis');
-const dotenv = require('dotenv');
-dotenv.load();
 const client = redis.createClient();
-client.auth(process.env.REDISPW);
 
 module.exports = client;
