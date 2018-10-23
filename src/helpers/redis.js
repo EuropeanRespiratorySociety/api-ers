189fd30f0cf31759d774dfea33cdeec78ad37e83
idealley <samuelpouyt@gmail.com>
2018-10-23 16:43:53 +0200

    Added Redis PW from ENV

--- src/helpers/redis.js
+++ src/helpers/redis.js
@@ -2,6 +2,7 @@ const redis = require('redis');
 const dotenv = require('dotenv');
 dotenv.load();
 const client = redis.createClient({
-  host: process.env.REDIS_HOST || '127.0.0.1'
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  password: process.env.REDISPW
 });
 module.exports = client;
