840035e29865d21020c36def8a8666603f1efcd6
idealley <samuelpouyt@gmail.com>
2018-11-27 09:21:12 +0100

    Cleaning up code

--- src/services/webhook/webhook.journals.js
+++ src/services/webhook/webhook.journals.js
@@ -5,7 +5,7 @@ const u = require('./webhook.utils');
 
 class Journals {
 
-  async upsertJournalAbstract (app, abstract, force = false) {
+  async upsertJournalAbstract(app, abstract, force = false) {
     console.log(chalk.cyan('[webhook]'), 'Receiving journal abstract...');
     const service = app.service('journals');
     let id;
@@ -15,14 +15,14 @@ class Journals {
     // to check for an error.
     let lastUpdate = m().subtract(10, 'minutes').format();
 
-    if(abstract.doi === '10.1183/09031936.00000309' && abstract.page_url==='http://err.ersjournals.com/content/18/112/125') {
-      return { status: 'this item has been arbitriraly discarded as is a DOI duplicate, it should be sorted eventually.' };
+    if (abstract.doi === '10.1183/09031936.00000309' && abstract.page_url === 'http://err.ersjournals.com/content/18/112/125') {
+      return { status: 'this item has been arbitriraly discarded as it is a DOI duplicate, it should be sorted eventually.' };
     }
 
     try {
-      if(abstract.canonical && abstract.pubmed_id === undefined) {
+      if (abstract.canonical && abstract.pubmed_id === undefined) {
         item = await service.find({
-          query: { 
+          query: {
             canonical: abstract.canonical
           }
         });
@@ -33,35 +33,35 @@ class Journals {
           }
         });
       }
-      if(item.data.length > 0) {
+      if (item.data.length > 0) {
         id = item.data[0]._id;
       }
 
       lastUpdate = await u.getAsync(`journal-abstract-${id}`);
     } catch (e) {
-      return {status: 'Error', message: e};
+      return { status: 'Error', message: e };
     }
 
     if (m(item.scrappedOn) > m(lastUpdate) || force) {
       if (abstract.article_type === '') {
-        console.log(chalk.cyan('>>> '), {id: id, status: 'Rejected - no article_type'});
-        return {id: id, status: 'Not inserted - no article_type'};
+        console.log(chalk.cyan('>>> '), { id: id, status: 'Rejected - no article_type' });
+        return { id: id, status: 'Not inserted - no article_type' };
       }
       if (id !== undefined) {
         await service.patch(id, abstract, { mongoose: { upsert: true } });
         await u.setAsync(`journal-abstract-${id}`, m().format());
-        console.log(chalk.cyan('>>> '), {id: id, status: 'Updated'});
-        return {id: id, status: 'Updated'};
+        console.log(chalk.cyan('>>> '), { id: id, status: 'Updated' });
+        return { id: id, status: 'Updated' };
       } else {
         const r = await service.create(abstract);
         id = r._id;
         await u.setAsync(`journal-abstract-${id}`, m().format());
-        console.log(chalk.cyan('>>> '), {id: id, status: 'Inserted'});
-        return {id: id, status: 'Inserted'};
+        console.log(chalk.cyan('>>> '), { id: id, status: 'Inserted' });
+        return { id: id, status: 'Inserted' };
       }
     }
-    console.log(chalk.cyan('>>> '), {id: id, status: 'Not updated'});
-    return {id: id, status: 'Not updated'};
+    console.log(chalk.cyan('>>> '), { id: id, status: 'Not updated' });
+    return { id: id, status: 'Not updated' };
   }
 
   async indexJournals(app, printErrors = false, force = false) {
@@ -71,7 +71,7 @@ class Journals {
     const limit = 100;
     const type = 'journals';
     const s = app.service(type);
-    
+
     // 1. Divide total by batches 
     // there is a lot in this table, lets get only the updated one..
     const lastJob = await u.getAsync(`last-${type}-indexed-job`);
@@ -79,16 +79,16 @@ class Journals {
       ? m(lastJob).format()
       : m().subtract(10, 'minutes').format();
 
-    const data = await s.find({ 
+    const data = await s.find({
       query: Object.assign(
-        {}, 
-        { 
+        {},
+        {
           $limit: limit,
           article_type: {
             $ne: ''
           }
-        }, 
-        force ? {} : { updatedAt: { $gte: time }}
+        },
+        force ? {} : { updatedAt: { $gte: time } }
       )
     });
     const firstBatch = data.data;
@@ -100,32 +100,32 @@ class Journals {
     console.log(chalk.cyan('[webhook]'), 'Indexing batch #1...');
     console.time('indexing');
     const r1 = await u.indexData(firstBatch, 'journals');
-    r1.map(i => result.push({item: i._id.toString(), stats: i}));
+    r1.map(i => result.push({ item: i._id.toString(), stats: i }));
 
     let i = 1;
-    for(i; i < batches; i++) {
+    for (i; i < batches; i++) {
       const b = await s.find({
         query: Object.assign(
-          {}, 
-          { 
+          {},
+          {
             $limit: limit, $skip: i * limit,
             article_type: {
               $ne: ''
             }
-          }, 
-          force ? {} : { updatedAt: { $gte: time }}
+          },
+          force ? {} : { updatedAt: { $gte: time } }
         )
       });
 
       console.log(chalk.cyan('[webhook]'), `Indexing batch #${i + 1}...`);
       const rn = await u.indexData(b.data, 'journals');
-      rn.map(i => result.push({item: i._id.toString(), stats: i}));
+      rn.map(i => result.push({ item: i._id.toString(), stats: i }));
     }
     console.timeEnd('indexing');
-    
+
     // 3. @TODO record failures and retry
-    
-    if(printErrors) {
+
+    if (printErrors) {
       const errors = result.filter(i => !u.isSucess(i));
       console.log('Errors: ', errors);
     }
