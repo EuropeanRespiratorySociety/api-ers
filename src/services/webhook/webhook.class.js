14547de95e72f44315e75b1c4136da548006c069
Florence Blocklet <florence.blocklet@ersnet.org>
2019-03-04 13:24:19 +0100

    Add nodemailer + Test new UAT env

--- src/services/webhook/webhook.class.js
+++ src/services/webhook/webhook.class.js
@@ -3,13 +3,17 @@ const h = require('./webhook.helpers');
 const j = require('./webhook.journals');
 const s = require('./webhook.static-pages');
 const c = require('./webhook.classify');
+const digest = require('./webhook.digest');
 const coordinates = require('./webhook.coordinates');
 const abstracts = require('./webhook.add.abstracts.classification');
 const cache = require('./webhook.cache');
 const errors = require('@feathersjs/errors');
+
 //  making sure it .env is loaded
 const dotenv = require('dotenv');
 dotenv.load();
+
+const notImplemented = 'no such processing available';
 
 class Service {
   constructor(options) {
@@ -31,35 +35,35 @@ class Service {
 
     /* eslint-disable indent */
     // prettier-ignore-start
-    return pw !== process.env.WPW
-      ? error()
-      : type === 'save-congress-sessions'
-        ? h.upsertSessions(this.app, congress, e, seeding, force)
-        : type === 'save-congress-presentations'
-          ? presentations(this.app, congress, e, seeding, force)
-          : type === 'save-congress-abstracts'
-            ? h.upsertAbstracts(this.app, congress, e, seeding, force)
-            : type === 'index-congress-abstracts'
-              ? h.indexCongress(this.app, 'abstracts', congress)
-              : type === 'index-congress-sessions'
-                ? h.indexCongress(this.app, 'sessions', congress)
-                : type === 'index-congress-presentations'
-                  ? h.indexCongress(this.app, 'presentations', congress)
-                  : type === 'index-ers-content'
-                    ? h.indexErsContent(this.app, 'ers:article', printErrors)
-                    : type === 'index-ers-static-content'
-                      ? s.index(this.app, 'ers:article', printErrors)
-                      : type === 'index-sb-content'
-                        ? h.indexErsContent(this.app, 'sb:article')
-                        : type === 'index-journals'
-                          ? j.indexJournals(this.app, printErrors, force)
-                          : type === 'classify-cloud-cms-content'
-                            ? classifyCloudCMS(this.app)
-                            : type === 'classify-journals'
-                              ? classifyJournals(this.app)
-                              : type === 'add-abstract-classification'
-                                ? abstracts(this.app)
-                                : 'no such processing available';
+    return pw !== process.env.WPW ?
+      error() :
+      type === 'save-congress-sessions' ?
+      h.upsertSessions(this.app, congress, e, seeding, force) :
+      type === 'save-congress-presentations' ?
+      presentations(this.app, congress, e, seeding, force) :
+      type === 'save-congress-abstracts' ?
+      h.upsertAbstracts(this.app, congress, e, seeding, force) :
+      type === 'index-congress-abstracts' ?
+      h.indexCongress(this.app, 'abstracts', congress) :
+      type === 'index-congress-sessions' ?
+      h.indexCongress(this.app, 'sessions', congress) :
+      type === 'index-congress-presentations' ?
+      h.indexCongress(this.app, 'presentations', congress) :
+      type === 'index-ers-content' ?
+      h.indexErsContent(this.app, 'ers:article', printErrors) :
+      type === 'index-ers-static-content' ?
+      s.index(this.app, 'ers:article', printErrors) :
+      type === 'index-sb-content' ?
+      h.indexErsContent(this.app, 'sb:article') :
+      type === 'index-journals' ?
+      j.indexJournals(this.app, printErrors, force) :
+      type === 'classify-cloud-cms-content' ?
+      classifyCloudCMS(this.app) :
+      type === 'classify-journals' ?
+      classifyJournals(this.app) :
+      type === 'add-abstract-classification' ?
+      abstracts(this.app) :
+      notImplemented;
     /* eslint-enable indent */
   }
 
@@ -67,7 +71,7 @@ class Service {
     const type = params.query.type;
     const pw = params.query.pw;
     const force = params.query.force == 'true' ? true : false;
-    // console.log(data)
+
     // console.log(params, 'pw: ', pw, ' ', process.env.WPW)
 
     // if (type === 'cache' || isCloudCMS(data)) {
@@ -75,15 +79,17 @@ class Service {
     // }
 
     /* eslint-disable indent */
-    return pw !== process.env.WPW && !isCloudCMS(data)
-      ? error()
-      : type === 'cache'
-        ? cache.clear(data)
-        : type === 'save-journal-abstract'
-          ? j.upsertJournalAbstract(this.app, data, true) // temporarily forcing
-          : type === 'coordinates'
-            ? await coordinates.save(await coordinates.generate(data), data) // eslint-disable-line no-console
-            : 'other method not yet implemented';
+    return pw !== process.env.WPW && !isCloudCMS(data) ?
+      error() :
+      type === 'cache' ?
+      cache.clear(data) :
+      type === 'save-journal-abstract' ?
+      j.upsertJournalAbstract(this.app, data, true) : // temporarily forcing
+      type === 'coordinates' ?
+      await coordinates.save(await coordinates.generate(data), data) :
+      type === 'digest-article' ?
+      digest.sendNotificationUpdate(data) : // eslint-disable-line no-console
+      'other method not yet implemented';
     /* eslint-enable indent */
   }
 
@@ -112,15 +118,21 @@ function error() {
 
 function presentations(app, congress, e, seeding, force) {
   h.upsertPresentations(app, congress, e, seeding, force);
-  return { message: 'This process runs for a long time, check the logs in ES to know where we are at' };
+  return {
+    message: 'This process runs for a long time, check the logs in ES to know where we are at'
+  };
 }
 
 function classifyCloudCMS(app) {
   c.classifyCloudCMSContent(app);
-  return { message: 'This process runs for a long time, check the logs to know where we are at' };
+  return {
+    message: 'This process runs for a long time, check the logs to know where we are at'
+  };
 }
 
 function classifyJournals(app) {
   c.classifyJournals(app);
-  return { message: 'This process runs for a long time, check the logs to know where we are at' };
+  return {
+    message: 'This process runs for a long time, check the logs to know where we are at'
+  };
 }
