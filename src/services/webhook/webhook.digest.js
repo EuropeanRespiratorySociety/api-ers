c173c36c5b46c9767c8650800c452d09d31b2011
Florence Blocklet <florence.blocklet@ersnet.org>
2019-03-05 09:27:16 +0100

    Fix eslint

--- src/services/webhook/webhook.digest.js
+++ src/services/webhook/webhook.digest.js
@@ -12,37 +12,34 @@ let deniedUsers = () => {
 class Digest {
   async sendNotificationUpdate(data) {
     const item = data._cloudcms.node.object;
-    console.log(JSON.stringify(item));
-    // if (deniedUsers().includes(item._system.modified_by_principal_id)) {
-    //   let html = '<p>Hello</p><p>This digest has been updated on CloudCMS:' + item.title + '</p>';
-    //   let text = 'Hello, this digest has been updated on CloudCMS:' + item.title;
-    //   let transporter = nodemailer.createTransport({
-    //     pool: true,
-    //     host: 'mailsrv3.ersnet.org',
-    //     port: 25,
-    //     secure: false
-    //   });
+    if (deniedUsers().includes(item._system.modified_by_principal_id)) {
+      let html = '<p>Hello</p><p>This digest has been updated on CloudCMS:' + item.title + '</p>';
+      let text = 'Hello, this digest has been updated on CloudCMS:' + item.title;
+      let transporter = nodemailer.createTransport({
+        pool: true,
+        host: 'mailsrv3.ersnet.org',
+        port: 25,
+        secure: false
+      });
 
-    //   // verify connection configuration
-    //   transporter.verify(function (error, success) {
-    //     if (error) {
-    //       console.log(error);
-    //     } else {
-    //       let message = {
-    //         from: 'webmaster@ersnet.org',
-    //         to: 'florence.blocklet@ersnet.org',
-    //         subject: 'CloudCMS: Digest Article Updated',
-    //         text: text,
-    //         html: html
-    //       };
-    //       transporter.sendMail(message, (err, info) => {
-    //         console.log(err);
-    //         console.log(info.envelope);
-    //         console.log(info.messageId);
-    //       });
-    //     }
-    //   });
-    //}
+      // verify connection configuration
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+        } else {
+          let message = {
+            from: 'webmaster@ersnet.org',
+            to: 'florence.blocklet@ersnet.org',
+            subject: 'CloudCMS: Digest Article Updated',
+            text: text,
+            html: html
+          };
+          transporter.sendMail(message, (err, info) => {
+            console.log(err);
+          });
+        }
+      });
+    }
   }
 }
 
