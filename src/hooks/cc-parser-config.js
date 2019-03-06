87cfe7290ad39996b5a13db2d13fa8b472f38790
Florence Blocklet <florence.blocklet@ersnet.org>
2019-03-06 11:38:44 +0100

    Fix: cc-parser-config - documents nodes

--- src/hooks/cc-parser-config.js
+++ src/hooks/cc-parser-config.js
@@ -25,7 +25,7 @@ module.exports = {
     'practicalInfo',
     'disclosure',
     'documents',
-    'rulesAndRegulation'
+    'rulesAndRegulations'
   ],
   dateProperties: [
     'notificationOfResults',
