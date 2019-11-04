4798ac16fe76283a2c7649cddf654d35d3c4fafa
fblocklet <43543975+fblocklet@users.noreply.github.com>
2019-11-04 16:07:45 +0100

    Fix: Added leadership role on leadership.map (TypeCode CHC, ScopeCode JMC)

--- src/services/leadership/leadership.map.js
+++ src/services/leadership/leadership.map.js
@@ -1,120 +1,123 @@
 module.exports = {
-  interests: [
-    {
-      code: 'CON',
-      description: 'Regular Paid Consultancy Work'
-    },
-    {
-      code: 'BEN',
-      description: 'Personal Benefits'
-    },
-    {
-      code: 'TRA',
-      description: 'Personal Travel Grants or expenses for conferences etc.'
-    },
-    {
-      code: 'SHA',
-      description: 'Shares in any Relevant Companies (excluding mutual funds)'
-    },
-    {
-      code: 'NOP',
-      description: 'Non-Personal Interests'
-    },
-    {
-      code: 'OTH',
-      description: 'Other Interests that may be seen as potential conflicts'
-    }
+  interests: [{
+    code: 'CON',
+    description: 'Regular Paid Consultancy Work'
+  },
+  {
+    code: 'BEN',
+    description: 'Personal Benefits'
+  },
+  {
+    code: 'TRA',
+    description: 'Personal Travel Grants or expenses for conferences etc.'
+  },
+  {
+    code: 'SHA',
+    description: 'Shares in any Relevant Companies (excluding mutual funds)'
+  },
+  {
+    code: 'NOP',
+    description: 'Non-Personal Interests'
+  },
+  {
+    code: 'OTH',
+    description: 'Other Interests that may be seen as potential conflicts'
+  }
   ],
-  order: [
-    {
-      TypeCode: 'P',
-      ScopeCode: '',
-      role: 'President'
-    },
-    {
-      TypeCode: 'PP',
-      ScopeCode: '',
-      role: 'Past President'
-    },
-    {
-      TypeCode: 'PE',
-      ScopeCode: '',
-      role: 'President Elect'
-    },
-    {
-      TypeCode: 'VP',
-      ScopeCode: '',
-      role: 'Vice President'
-    },
-    {
-      TypeCode: 'SG',
-      ScopeCode: '',
-      role: 'Secretary General'
-    },
-    {
-      TypeCode: 'T',
-      ScopeCode: '',
-      role: 'Treasurer'
-    },
-    {
-      TypeCode: 'ERSSCCH',
-      ScopeCode: '',
-      role: 'Education Council Chair'
-    },
-    {
-      TypeCode: 'PC',
-      ScopeCode: '',
-      role: 'Science Council Chair'
-    },
-    {
-      TypeCode: 'CHC',
-      ScopeCode: 'ADVC',
-      role: 'Advocacy Council Chair'
-    },
-    {
-      TypeCode: 'CHC',
-      ScopeCode: 'ELF',
-      role: 'ELF Council Chair'
-    },
-    {
-      TypeCode: 'CHC',
-      ScopeCode: 'PUBC',
-      role: 'Publication Committee Chair'
-    },
-    {
-      TypeCode: 'SGE',
-      ScopeCode: '',
-      role: 'Secretary General Elect'
-    },
-    {
-      TypeCode: 'TE',
-      ScopeCode: '',
-      role: 'Treasurer Elect'
-    },
-    {
-      TypeCode: 'SCE',
-      ScopeCode: '',
-      role: 'Education Council Chair Elect'
-    },
-    {
-      TypeCode: 'SCCE',
-      ScopeCode: '',
-      role: 'Science Council Chair Elect'
-    },
-    {
-      TypeCode: 'ADVCCE',
-      ScopeCode: '',
-      role: 'Advocacy Council Chair Elect'
-    },
-    {
-      TypeCode: 'ELFCE',
-      ScopeCode: '',
-      role: 'ELF Council Chair Elect'
-    },
-    {
-      TypeCode: 'PCCE',
-      ScopeCode: '',
-      role: 'Publication Committee Chair Elect'
-    }
+  order: [{
+    TypeCode: 'P',
+    ScopeCode: '',
+    role: 'President'
+  },
+  {
+    TypeCode: 'PP',
+    ScopeCode: '',
+    role: 'Past President'
+  },
+  {
+    TypeCode: 'PE',
+    ScopeCode: '',
+    role: 'President Elect'
+  },
+  {
+    TypeCode: 'VP',
+    ScopeCode: '',
+    role: 'Vice President'
+  },
+  {
+    TypeCode: 'SG',
+    ScopeCode: '',
+    role: 'Secretary General'
+  },
+  {
+    TypeCode: 'T',
+    ScopeCode: '',
+    role: 'Treasurer'
+  },
+  {
+    TypeCode: 'ERSSCCH',
+    ScopeCode: '',
+    role: 'Education Council Chair'
+  },
+  {
+    TypeCode: 'PC',
+    ScopeCode: '',
+    role: 'Science Council Chair'
+  },
+  {
+    TypeCode: 'CHC',
+    ScopeCode: 'ADVC',
+    role: 'Advocacy Council Chair'
+  },
+  {
+    TypeCode: 'CHC',
+    ScopeCode: 'ELF',
+    role: 'ELF Council Chair'
+  },
+  {
+    TypeCode: 'CHC',
+    ScopeCode: 'PUBC',
+    role: 'Publication Committee Chair'
+  },
+  {
+    TypeCode: 'CHC',
+    ScopeCode: 'JMC',
+    role: 'Early Career Members Committee'
+  },
+  {
+    TypeCode: 'SGE',
+    ScopeCode: '',
+    role: 'Secretary General Elect'
+  },
+  {
+    TypeCode: 'TE',
+    ScopeCode: '',
+    role: 'Treasurer Elect'
+  },
+  {
+    TypeCode: 'SCE',
+    ScopeCode: '',
+    role: 'Education Council Chair Elect'
+  },
+  {
+    TypeCode: 'SCCE',
+    ScopeCode: '',
+    role: 'Science Council Chair Elect'
+  },
+  {
+    TypeCode: 'ADVCCE',
+    ScopeCode: '',
+    role: 'Advocacy Council Chair Elect'
+  },
+  {
+    TypeCode: 'ELFCE',
+    ScopeCode: '',
+    role: 'ELF Council Chair Elect'
+  },
+  {
+    TypeCode: 'PCCE',
+    ScopeCode: '',
+    role: 'Publication Committee Chair Elect'
+  }
   ]
 };
